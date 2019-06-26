6c484adfe96b542b5d92d3383be791a1ee4920c3
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-06-26 01:06:10 -0400

    commit con baseviewmodel

---
+++ App1/App1/App1/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace App1.ViewModels
+{
+    public class MainViewModel
+    {
+        #region VieModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+    }
+}
+
